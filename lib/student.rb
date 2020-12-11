a23126fa3f93dbd68813d0f504129606cfb89b6e
Benjamin Cheng <bencheng200@gmail.com>
2020-12-11 19:13:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :id, :grade, :name
+  attr_reader :id, :grade, :name
 
   def initialize(id = nil, name, grade)
     @id = id
