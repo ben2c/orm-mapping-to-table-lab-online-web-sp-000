2817d73cce77c4e7f7ef11f68d07aa5ade37b5f7
Benjamin Cheng <bencheng200@gmail.com>
2020-12-11 19:25:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   attr_reader :id
   attr_accessor :grade, :name
 
-  def initialize(id = nil, name, grade)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
